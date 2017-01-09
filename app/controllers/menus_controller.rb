a0fb4a16236bec4289b79a88ae93fddcfe9af55f
rymcmahon <toedragdangle9@gmail.com>
2017-01-09 13:56:21 -0600

    added item attributes to menu strong params and built menu show

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,8 +1,14 @@
 class MenusController < ApplicationController
+
+  def show
+    @menu = Menu.find(params[:id])
+  end
+
   def new
     @menu = Menu.new
     @menu.sections.build
   end
+
   def create
     @menu = Menu.new(menu_params)
     if @menu.save
@@ -14,6 +20,6 @@ class MenusController < ApplicationController
 
   private
   def menu_params
-    params.require(:menu).permit(:name, sections_attributes: [:id, :name, :_destroy])
+    params.require(:menu).permit(:name, sections_attributes: [:id, :name, :_destroy, items_attributes: [:id, :name, :description, :price, :available, :_destroy]])
   end
 end
