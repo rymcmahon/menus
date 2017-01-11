c6c474d54ee7671308983d66a4c54bb4224a797e
rymcmahon <toedragdangle9@gmail.com>
2017-01-11 13:53:15 -0600

    formatted menu form nesting

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,12 +1,10 @@
 class MenusController < ApplicationController
-
   def show
     @menu = Menu.find(params[:id])
   end
 
   def new
     @menu = Menu.new
-    @menu.sections.build
   end
 
   def edit
@@ -33,6 +31,7 @@ class MenusController < ApplicationController
   end
 
   private
+
   def menu_params
     params.require(:menu).permit(:name, sections_attributes: [:id, :name, :_destroy, items_attributes: [:id, :name, :description, :price, :available, :_destroy]])
   end
